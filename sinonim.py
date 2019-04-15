8b8a649bdf496927abf26f0b09687113592e79e3
xinzuan <47246543+xinzuan@users.noreply.github.com>
2019-04-15 10:19:38 +0700

    akan ditambahkan kamusnya

---
+++ sinonim.py
@@ -0,0 +1,21 @@
+import json
+# sumber thesaurus : https://theindonesianwriters.files.wordpress.com/2011/04/kamus-tesaurus_bahasa-indonesia.pdf
+
+def load(filename):	
+	with open(filename) as data_file:
+		data = json.load(data_file)	
+
+	return data
+
+# load dictionary
+mydict = load('dict.json')
+
+def getSinonim(word):
+	if word in mydict.keys():
+		return mydict[word]['sinonim']
+	else:
+		return []
+
+
+
+print (getSinonim('pola'))
