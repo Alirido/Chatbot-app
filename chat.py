47f3684f1b06a923b9bfb040dbab80cc3c9e9991
EdwardAJ <edward.jaya31@gmail.com>
2019-04-18 23:56:25 +0700

    Synonym handling + not found handling

--- chat.py
+++ chat.py
@@ -1,43 +1,115 @@
+# =============================================================
 #Program BACKEND untuk membaca pertanyaan dari file eksternal.
+# =============================================================
+
 import bmV2
 from kmp_algo import KMP_algo
+from tesaurus import getSinonim
+
+# =============================================================================
+# Fungsi untuk mengatur confidence level pertanyaan dari sinonim maupun bukan.
+# =============================================================================
+def setConfValueSynonym(tipe, conf_value, question, input_user_byword, idx):
+        #tipe = 1: KMP, tipe = 2: BoyerMoore, tipe = 3: Regex
+        if (tipe == 1):
+                #Cari by_word, yaitu kata perkata dari input_user_byword.
+                for byword in input_user_byword:
+                        conf_value_el_word = 0 ; conf_value_el_synonym = 0
+                        is_found_word = getAlgo(tipe,question,byword) #Panggil algo yang paling sesuai.
+                        #Ditemukan, maka conf_value diupdate.
+                        if (is_found_word != -1):
+                                conf_value_el_word =  min(len(byword),len(question))/ max(len(byword),len(question))
+                        
+                        synonym_list = getSinonim(byword)
+                        for synonym in synonym_list:
+                                is_found_word = getAlgo(tipe,question,byword)
+                                #Ditemukan
+                                if (is_found_word != -1):
+                                        conf_value_el_synonym = max(min(len(synonym),len(question))/ max(len(synonym),len(question)),conf_value_el_synonym) 
+
+                        #Bandingkan confidence value kata asli vs synonym, lalu update confidence value.
+                        conf_value[idx] = conf_value[idx] + max(conf_value_el_word, conf_value_el_synonym)
 
 
-#Baca file eksternal pertanyaan.txt
-file= open('pertanyaan.txt',"r")
+# =============================================================================
+# Fungsi untuk mendapat tipe algoritma yang diinginkan (KMP, BM, Regex)
+# =============================================================================
+def getAlgo(tipe, question, byword):
+        if (tipe == 1):
+                return KMP_algo(max(question, byword), min (question,byword))
+
+
+# =============================================================================
+# PROGRAM UTAMA BERJALAN DARI SINI
+# =============================================================================
+        
+file= open('pertanyaan.txt',"r") #Baca file eksternal pertanyaan.txt
 db_question = []
 
+# =============================================================================
 #Setiap pertanyaan dimasukkan ke dalam list pertanyaan (db_question : database)
+# =============================================================================
 for question_line in file:
-        question_line = question_line.strip() #Menghilangkan '\n'
+        question_line = question_line.replace(' ','') #Hapus semua space
+        question_line = question_line.replace('?','') #Hapus semua question mark
+        question_line = question_line.strip() #Hapus'\n'
         db_question.append(question_line)
+file.close()
+#print(db_question)
 
-file.close() #close file 
 
-
-
-#Baca file eksternal jawaban.txt
-file = open('jawaban.txt', "r")
+file = open('jawaban.txt', "r")  #Baca file eksternal jawaban.txt
 db_answer = []
 
-#Setiap jawaban dimasukkan ke dalam list jawaban (db_answer: database)
+# =============================================================================
+# Setiap jawaban dimasukkan ke dalam list jawaban (db_answer: database)
+# =============================================================================
 for answer_line in file:
-        answer_line = answer_line.strip() #Menghilangkan '\n'
+        answer_line = answer_line.strip()
         db_answer.append(answer_line)
-
 file.close()
 
+input_user = input() #Dapatkan input user
+input_user = input_user.replace('?','') #Menghapus '?'
+input_user_byword = input_user.split() #Menghapus space dari pertanyaan yang diinput oleh user, menjadi list of words.
+input_user = input_user.replace(' ','')
+print(input_user_byword)
 
-#Dapatkan input user
-input_user = input()
-
-#Lanjutkan dengan string matching. Cari dengan KMP algorithm:
+# =============================================================================
+# Lanjutkan dengan string matching. Cari dengan KMP algorithm:
+# =============================================================================
 idx = 0
+#conf_value = dict.fromkeys([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+
+conf_value = {key : 0 for key in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}
+
 for question in db_question:
-        is_found = KMP_algo(question, input_user)
+        if (len(question) > len(input_user)):
+                is_found = KMP_algo(question, input_user)
+        else:
+                is_found = KMP_algo(input_user, question)
         #Jika ditemukan
         if (is_found != -1):
                 #Kalau tingkat kecocokan di atas 90%
                 if ((len(input_user) / len(question) >= 0.9)):
                         print(db_answer[idx])
+                        break
+                            
         idx += 1
+
+#Jika tidak ditemukan
+idx = 0
+if (is_found == -1):
+        for question in db_question:
+                setConfValueSynonym(1, conf_value, question, input_user_byword,idx)
+                idx += 1
+        #Mencari key yang memiliki nilai maksimum dari dictionary
+        print(conf_value)
+        idx_value_max = max(conf_value, key = lambda x: conf_value.get(x) )
+        if (conf_value[idx_value_max] > 0.5):
+                idx = max(conf_value, key = lambda x: conf_value.get(x) )
+                print(db_answer[idx])
+        else:
+                print("Rephrase the question!")
+
+
