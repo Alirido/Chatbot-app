9adc018628c1d6a31ff030b2097217e37da44772
EdwardAJ <edward.jaya31@gmail.com>
2019-04-18 20:08:38 +0700

    Fix bug + adding 90% confidence level

--- chat.py
+++ chat.py
@@ -1,38 +1,43 @@
 #Program BACKEND untuk membaca pertanyaan dari file eksternal.
 import bmV2
+from kmp_algo import KMP_algo
 
-#Mendapat input pertanyaan dari pengguna
-input_user = input()
 
-#Baca file eksternal
-file = open('pertanyaan.txt',"r")
-
-#Membaca setiap pertanyaan di dalam pertanyaan.txt
-#db = database
+#Baca file eksternal pertanyaan.txt
+file= open('pertanyaan.txt',"r")
 db_question = []
-idx_question = 0
+
+#Setiap pertanyaan dimasukkan ke dalam list pertanyaan (db_question : database)
 for question_line in file:
         question_line = question_line.strip() #Menghilangkan '\n'
         db_question.append(question_line)
-        idx_question += 1
 
-#Setiap pertanyaan dimasukkan ke dalam list pertanyaan (db_question)
-#Lanjutkan dengan string matching.
-
-#Cari dengan KMP algorithm:
+file.close() #close file 
 
 
 
+#Baca file eksternal jawaban.txt
+file = open('jawaban.txt', "r")
+db_answer = []
+
+#Setiap jawaban dimasukkan ke dalam list jawaban (db_answer: database)
+for answer_line in file:
+        answer_line = answer_line.strip() #Menghilangkan '\n'
+        db_answer.append(answer_line)
+
+file.close()
 
 
-'''
-for c in q:
-    found =bmV2.BM(c,input_user)
-    if found:
-        print(q[input_user])
-        break
-'''
-#print ("Pattern \t:",input_user)
+#Dapatkan input user
+input_user = input()
 
-
-
+#Lanjutkan dengan string matching. Cari dengan KMP algorithm:
+idx = 0
+for question in db_question:
+        is_found = KMP_algo(question, input_user)
+        #Jika ditemukan
+        if (is_found != -1):
+                #Kalau tingkat kecocokan di atas 90%
+                if ((len(input_user) / len(question) >= 0.9)):
+                        print(db_answer[idx])
+        idx += 1
--- kmp_algo.py
+++ kmp_algo.py
@@ -2,8 +2,8 @@
 #Mengembalikan indeks ditemukannya pattern.
 def KMP_algo(text, pattern):
     #Cari panjang dari text dan pattern, biasanya panjang pattern < text.
-    text_len = text.len()
-    pattern_len = pattern.len()
+    text_len = len(text)
+    pattern_len = len(pattern)
     #Menghitung fungsi pinggiran dari KMP.
     func = computeFunc(pattern)
     i = 0 ; j = 0
@@ -25,12 +25,11 @@ def KMP_algo(text, pattern):
 #Fungsi pinggiran dari KMP, pre-processor dari Pattern.
 #Mengembalikan array yang berisi nilai indeks jika terjadi mismatch.
 def computeFunc(pattern):
-    #Array fungsi tersebut, memiliki panjang sebanyak panjang pattern.
-    func = []
+    pattern_len = len(pattern)
+    func = [None] * pattern_len
     func[0] = 0 #sudah pasti 0
 
     #Hitung func untuk indeks >=1
-    pattern_len = pattern.len()
     j = 0 ; func_idx = 1
     while (func_idx < pattern_len):
         #Kasus prefix dan suffix sama.
